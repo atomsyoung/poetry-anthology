f0590d4679b5efba6ade8e75ac3b94daa2ed7e87
xkss <yangqiju0@gmail.com>
2018-03-29 16:21:08 +0800

    update

--- amWiki/mounts/home-240.js
+++ amWiki/mounts/home-240.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# 古今中外诗选\r\n\r\n一个充满个人喜好的诗歌集子。\r\n\r\nby [YangQiju.com](https://www.yangqiju.com)\r\n","timestamp":1521211689309}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# 古今中外诗选\r\n\r\n一个充满个人喜好的诗歌集子。\r\n\r\nby [YangQiju.com](https://www.yangqiju.com)\r\n","timestamp":1522311622987}
